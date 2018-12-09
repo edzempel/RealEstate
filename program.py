be617445df14bef38a0dfbd5930355ef8d7da162
EdDesk <edward.zempel@gmail.com>
2018-12-08 23:40:02 -0600

    add data_types.py class Purchases for handling each row as an object with the columns from one row of the csv file as the attributes

--- program.py
+++ program.py
@@ -1,6 +1,9 @@
 import csv
 import os
 
+from data_types import Purchase
+
+
 def main():
     print_header()
     filename = get_data_file()
@@ -20,38 +23,18 @@ def get_data_file():
     base_folder = os.path.dirname(__file__)
     return os.path.join(base_folder, 'data', 'SacramentoRealEstateTransactions2008.csv')
 
+
 def load_file(filename):
     with open(filename, 'r', encoding='utf-8') as fin:
-
-# use DictReader to create more resilient code
-# using a key instead of an idex allows the columns to be added or deleted form the csv
+        # use DictReader to create more resilient code
+        # using a key instead of an idex allows the columns to be added or deleted form the csv
         reader = csv.DictReader(fin)
+        purchases = []
         for row in reader:
-            print(type(row), row)
-            print("Bed count: {}".format(row['beds']))
+            p = Purchase.create_from_dict(row)
+            purchases.append(p)
 
-
-        # header = fin.readline().strip()
-        # reader = csv.reader(fin, delimiter=',')
-        # for row in reader:
-        #     print(type(row), row)
-        #     beds = row[4]
-
-
-
-
-# def load_file_basic(filename):
-#     with open(filename, 'r', encoding='utf-8') as fin:
-#         header = fin.readline().strip()
-#         print('found header: ' + header)
-#
-#         lines = []
-#         for line in fin:
-#             line_data = line.strip().split(',')
-#             lines.append(line_data)
-#
-#             print(lines[:5])
-
+        return purchases
 
 def query_data(data):
     pass
